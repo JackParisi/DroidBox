a5f50ee3e6ac83465d60ca8b6141f776dff07c2f
Giacomo Parisi <giacomoparisi95@gmail.com>
2017-07-30 17:21:43 +0200

    Fix wrapper service

--- droidbox/src/main/java/com/github/jackparisi/droidbox/wrapper/DroidWrapperService.kt
+++ droidbox/src/main/java/com/github/jackparisi/droidbox/wrapper/DroidWrapperService.kt
@@ -17,12 +17,12 @@ abstract class DroidWrapperService(val context: Context) {
     var toolbarWrapper = ToolbarDroidWrapper()
 
     protected fun <W : ViewDataBinding> wrapErrorLayout(viewModel: DroidViewModel, root: View, wrapperClass: KClass<W>): View {
-        val wrapperRoot = createErrorWrapper(wrapperClass)
+        val wrapperRoot = createErrorWrapper(viewModel, wrapperClass)
         return errorWrapper.wrapLayout(viewModel, root, wrapperRoot, context)
     }
 
     protected fun <W : ViewDataBinding> wrapLoadingLayout(viewModel: DroidViewModel, root: View, wrapperClass: KClass<W>): View {
-        val wrapperRoot = createLoadingWrapper(wrapperClass)
+        val wrapperRoot = createLoadingWrapper(viewModel, wrapperClass)
         return loadingWrapper.wrapLayout(viewModel, root, wrapperRoot, context)
     }
 
@@ -31,11 +31,11 @@ abstract class DroidWrapperService(val context: Context) {
         return toolbarWrapper.wrapLayout(viewModel, root, wrapperRoot, context)
     }
 
-    protected abstract fun <W : ViewDataBinding> createErrorWrapper(wrapperClass: KClass<W>): View?
+    protected abstract fun <W : ViewDataBinding> createErrorWrapper(viewModel: DroidViewModel, wrapperClass: KClass<W>): View?
 
-    protected abstract fun <W : ViewDataBinding> createLoadingWrapper(wrapperClass: KClass<W>): View?
+    protected abstract fun <W : ViewDataBinding> createLoadingWrapper(viewModel: DroidViewModel, wrapperClass: KClass<W>): View?
 
-    protected abstract fun <W : ViewDataBinding> createToolbarWrapper(toolbarViewModel: DroidViewModel, wrapperClass: KClass<W>): View?
+    protected abstract fun <W : ViewDataBinding> createToolbarWrapper(viewModel: DroidViewModel, wrapperClass: KClass<W>): View?
 
     fun onViewDestroy(){
         errorWrapper.removeCallback()
