694b8d63a12d857dd67d009be32b825949d7bba1
Giacomo Parisi <giacomoparisi95@gmail.com>
2017-10-06 17:44:41 +0200

    Add docs for NavigationController

--- droidbox/src/main/java/com/github/jackparisi/droidbox/architecture/model/ui/DroidNavigationController.kt
+++ droidbox/src/main/java/com/github/jackparisi/droidbox/architecture/model/ui/DroidNavigationController.kt
@@ -11,6 +11,16 @@ import android.support.v4.app.FragmentActivity
 
 class DroidNavigationController {
 
+    /**
+     *
+     * Open new activity on top and optionally close the current activity
+     * and/or send a bundle to the next activity
+     *
+     * @param currentActivity The current activity that want to open the new page
+     * @param nextActivity The class type of the activity that will be opened
+     * @param bundle The bundle that will wi pass to the next activity
+     * @param finish Set it to true if you want to remove the current activity from the stack
+     */
     fun openActivity(currentActivity: FragmentActivity, nextActivity: Class<*>, bundle: Bundle? = null, finish: Boolean = false){
         val intent = Intent(currentActivity, nextActivity)
         bundle?.let { intent.putExtras(bundle) }
