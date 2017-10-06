c86a50c831b098223a11262fd37d6199aa986914
Giacomo Parisi <giacomoparisi95@gmail.com>
2017-10-06 16:08:06 +0200

    Add docs for DroidObservableData

--- droidbox/src/main/java/com/github/jackparisi/droidbox/architecture/model/data/DroidObservableData.kt
+++ droidbox/src/main/java/com/github/jackparisi/droidbox/architecture/model/data/DroidObservableData.kt
@@ -11,8 +11,16 @@ import android.databinding.ObservableField
 
 class DroidObservableData<T : ObservableField<*>>(var data: T) : LiveData<T>() {
 
+    // Callback for property changed event
     private var callback: Observable.OnPropertyChangedCallback? = null
 
+    /**
+     *
+     * Set the callback for the value that will be called when the value is updated,
+     * and removed when the context to which it is tied ends
+     *
+     * @param callback The callback that will be bind to the value
+     */
     fun setCallback(callback: Observable.OnPropertyChangedCallback) {
         this.callback = callback
         data.addOnPropertyChangedCallback(callback)
