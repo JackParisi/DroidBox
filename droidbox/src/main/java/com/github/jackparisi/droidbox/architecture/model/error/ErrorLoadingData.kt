ee656e231b02d812dba83c45439a72814de39ea1
Giacomo Parisi <giacomoparisi95@gmail.com>
2017-10-06 16:16:53 +0200

    Add docs for ErrorLoadingData

--- droidbox/src/main/java/com/github/jackparisi/droidbox/architecture/model/error/ErrorLoadingData.kt
+++ droidbox/src/main/java/com/github/jackparisi/droidbox/architecture/model/error/ErrorLoadingData.kt
@@ -8,13 +8,23 @@ import android.databinding.ObservableField
  * https://github.com/JackParisi
  */
 interface ErrorLoadingData {
+
+    // True if loading view is needed
     val loading: ObservableBoolean
 
+    // True if error view is needed
     val error: ObservableBoolean
 
-    fun retryFromUi()
-
+    // The string value for the error message
     val errorMessage: ObservableField<String>
 
+    // The string value for the error button message
     val errorButtonMessage: ObservableField<String>
+
+    /**
+     *
+     * The retry function that need to be called when the user
+     * wants to retry an action due to an error
+     */
+    fun retryFromUi()
 }
