575d19d2fd7f5ae22386f8750b8d30acb3abce81
Giacomo Parisi <giacomoparisi95@gmail.com>
2017-10-09 14:47:08 +0200

    Add docs for DroidRepository

--- droidbox/src/main/java/com/github/jackparisi/droidbox/network/DroidRepository.kt
+++ droidbox/src/main/java/com/github/jackparisi/droidbox/network/DroidRepository.kt
@@ -5,10 +5,23 @@ package com.github.jackparisi.droidbox.network
  * https://github.com/JackParisi
  */
 
+/**
+ *
+ * Repository class that takes care of managing how to obtain a specific data stream
+ * through the help of a data provider
+ */
 abstract class DroidRepository<P : DroidDataProvider<*>> {
 
+    // The data provider reference for the repository
     abstract var dataProvider: P
 
+    /**
+     *
+     * Setup the data for the network or database requests in the data provider
+     *
+     * @param databaseData The database data map to set for requests
+     * @param networkData The network data map to set for requests
+     */
     fun setData(databaseData: Map<String, String>? = null, networkData: Map<String, String>? = null) {
         dataProvider.setData(databaseData, networkData)
     }
