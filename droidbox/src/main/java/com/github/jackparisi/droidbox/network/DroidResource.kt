9a2fc42a99a61999e74a47764d73178d69025e97
Giacomo Parisi <giacomoparisi95@gmail.com>
2017-10-09 11:34:02 +0200

    Add docs to DroidResource

--- droidbox/src/main/java/com/github/jackparisi/droidbox/network/DroidResource.kt
+++ droidbox/src/main/java/com/github/jackparisi/droidbox/network/DroidResource.kt
@@ -5,13 +5,48 @@ package com.github.jackparisi.droidbox.network
  * https://github.com/JackParisi
  */
 
-open class DroidResource<T>(val data: T? = null, val errorMessage: String? = null, val isFetchedFromNetwork: Boolean = true) {
+/**
+ *
+ * Wrapper class for repository's response objects (network or database).
+ * Allows to add additional data and information to a repository's response objects
+ *
+ * @param data The response data
+ * @param errorMessage The request's error message
+ * @param isFetchedFromNetwork True if the data was fetched form network
+ */
+open class DroidResource<out T>(
+        val data: T? = null,
+        val errorMessage: String? = null,
+        val isFetchedFromNetwork: Boolean = true) {
 
-    class NetworkResource<T>(data: T) : DroidResource<T>(data = data)
+    /**
+     *
+     * Used for a positive response from the network (200)
+     *
+     * @param data The response data
+     */
+    class NetworkResource<out T>(data: T) : DroidResource<T>(data = data)
 
+    /**
+     *
+     * Used for a error response from the network
+     *
+     * @param errorMessage The error message returned from the network request
+     */
     class NetworkErrorResource(errorMessage: String?) : DroidResource<Nothing>(data = null, errorMessage = errorMessage) {
-        constructor(t: Throwable) : this(t.message ?: "")
+
+        /**
+         *
+         * @param throwable The exception raised from the network request
+         */
+        constructor(throwable: Throwable) : this(throwable.message ?: "")
     }
 
-    class DatabaseResource<T>(data: T) : DroidResource<T>(data = data, isFetchedFromNetwork = false)
+    /**
+     *
+     * Used for a response from the database after load data from it
+     *
+     * @param data The response data
+     */
+    class DatabaseResource<out T>(data: T) : DroidResource<T>(data = data, isFetchedFromNetwork = false)
 }
