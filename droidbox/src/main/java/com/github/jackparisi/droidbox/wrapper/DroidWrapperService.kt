04c2a2cd5a9119b6d11ba00a35e12ed7b8ae74be
Giacomo Parisi <giacomoparisi95@gmail.com>
2017-10-23 11:45:12 +0200

    Add docs for droid wrapper service

--- droidbox/src/main/java/com/github/jackparisi/droidbox/wrapper/DroidWrapperService.kt
+++ droidbox/src/main/java/com/github/jackparisi/droidbox/wrapper/DroidWrapperService.kt
@@ -14,12 +14,26 @@ import kotlin.reflect.KClass
  * Created by Giacomo Parisi on 25/07/17.
  * https://github.com/JackParisi
  */
+
+/**
+ * Service that perform the wrap action for the standard DroidBox wrapper
+ */
 abstract class DroidWrapperService(val context: Context) {
 
     private var errorWrapper = ErrorDroidWrapper()
     private val loadingWrapper = LoadingDroidWrapper()
     private var toolbarWrapper = ToolbarDroidWrapper()
 
+    /**
+     *
+     * Wrap the error layout with the root view
+     *
+     * @param viewModel DroidViewModel used for binding
+     * @param root The root view that need the error view
+     * @param wrapperClass The class of the wrapper (for take different error view based on the wrapper class)
+     *
+     * @return The root view wrapped with the error view
+     */
     fun <W : ViewDataBinding> wrapErrorLayout(viewModel: DroidViewModel, root: View, wrapperClass: KClass<W>): View {
         val wrapperRoot = createErrorWrapper(viewModel, wrapperClass)
         return errorWrapper.wrapLayout(
@@ -37,6 +51,16 @@ abstract class DroidWrapperService(val context: Context) {
 
     }
 
+    /**
+     *
+     * Wrap the loading layout with the root view
+     *
+     * @param viewModel DroidViewModel used for binding
+     * @param root The root view that need the loading view
+     * @param wrapperClass The class of the wrapper (for take different loading view based on the wrapper class)
+     *
+     * @return The root view wrapped with the loading view
+     */
     fun <W : ViewDataBinding> wrapLoadingLayout(viewModel: DroidViewModel, root: View, wrapperClass: KClass<W>): View {
         val wrapperRoot = createLoadingWrapper(viewModel, wrapperClass)
         return loadingWrapper.wrapLayout(
@@ -53,6 +77,16 @@ abstract class DroidWrapperService(val context: Context) {
         )
     }
 
+    /**
+     *
+     * Wrap the toolbar layout with the root view
+     *
+     * @param viewModel DroidViewModel used for binding
+     * @param root The root view that need the toolbar
+     * @param wrapperClass The class of the wrapper (for take different toolbar layout based on the wrapper class)
+     *
+     * @return The root view wrapped with the toolbar
+     */
     fun <W : ViewDataBinding> wrapToolbarLayout(viewModel: DroidViewModel, root: View, toolbarDroidConfigurator: ToolbarDroidConfigurator, wrapperClass: KClass<W>, overPageLayout: Boolean): View {
         val wrapperRoot = createToolbarWrapper(viewModel, toolbarDroidConfigurator, wrapperClass)
         return toolbarWrapper.wrapLayout(
@@ -70,12 +104,45 @@ abstract class DroidWrapperService(val context: Context) {
         )
     }
 
+    /**
+     *
+     * Create the error view based on the wrapper class
+     *
+     * @param viewModel The view model used for binding
+     * @param wrapperClass The wrapper class that need the error view
+     *
+     * @return The error view
+     */
     protected abstract fun <W : ViewDataBinding> createErrorWrapper(viewModel: DroidViewModel, wrapperClass: KClass<W>): View?
 
+    /**
+     *
+     * Create the error view based on the wrapper class
+     *
+     * @param viewModel The view model used for binding
+     * @param wrapperClass The wrapper class that need the loading view
+     *
+     * @return The loading view
+     */
     protected abstract fun <W : ViewDataBinding> createLoadingWrapper(viewModel: DroidViewModel, wrapperClass: KClass<W>): View?
 
+
+    /**
+     *
+     * Create the error view based on the wrapper class
+     *
+     * @param viewModel The view model used for binding
+     * @param wrapperClass The wrapper class that need the toolbar
+     *
+     * @return The toolbar view
+     */
     protected abstract fun <W : ViewDataBinding> createToolbarWrapper(viewModel: DroidViewModel, toolbarDroidConfigurator: ToolbarDroidConfigurator, wrapperClass: KClass<W>): View?
 
+
+    /**
+     *
+     * Call this method for unbind the wrapper view from the view model
+     */
     fun onViewDestroy() {
         errorWrapper.removeCallback()
         loadingWrapper.removeCallback()
