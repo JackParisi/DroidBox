dbb18c71cd118163e8b070777ed0f430d5336769
Giacomo Parisi <giacomoparisi95@gmail.com>
2017-10-23 13:05:49 +0200

    Calculate padding for toolbar wrapper with overLayoutPage settings enable

--- droidbox/src/main/java/com/github/jackparisi/droidbox/wrapper/toolbar/ToolbarDroidWrapper.kt
+++ droidbox/src/main/java/com/github/jackparisi/droidbox/wrapper/toolbar/ToolbarDroidWrapper.kt
@@ -1,11 +1,14 @@
 package com.github.jackparisi.droidbox.wrapper.toolbar
 
 import android.view.View
+import android.view.ViewGroup
+import android.view.ViewTreeObserver
 import android.widget.FrameLayout
 import android.widget.LinearLayout
 import com.github.jackparisi.droidbox.wrapper.DroidWrapper
 import com.github.jackparisi.droidbox.wrapper.DroidWrapperSettings
 import timber.log.Timber
+
 
 /**
  * Created by Giacomo Parisi on 30/06/2017.
@@ -17,7 +20,7 @@ import timber.log.Timber
  * DroidWrapper for toolbar.
  * Put the page layout below the toolbar using a linearLayout
  */
-class ToolbarDroidWrapper : DroidWrapper(){
+class ToolbarDroidWrapper : DroidWrapper() {
 
     override fun <T : DroidWrapperSettings> wrapLayout(settings: T): View {
         if (settings is ToolbarDroidSettings) {
@@ -30,6 +33,26 @@ class ToolbarDroidWrapper : DroidWrapper(){
                     Timber.e("Toolbar wrapper is null")
                 }
                 frameLayout.addView(settings.wrapperLayout)
+                settings.wrapperLayout?.viewTreeObserver?.addOnGlobalLayoutListener(
+                        object : ViewTreeObserver.OnGlobalLayoutListener {
+                            override fun onGlobalLayout() {
+                                settings.wrapperLayout.viewTreeObserver.removeOnGlobalLayoutListener(this)
+                                val height = settings.wrapperLayout.measuredHeight
+
+                                if (settings.pageLayout is ViewGroup) {
+                                    if (settings.pageLayout.childCount > 0) {
+                                        val params = settings.pageLayout.getChildAt(0).layoutParams
+                                        if (params is ViewGroup.MarginLayoutParams) {
+                                            params.topMargin = params.topMargin + height
+                                            settings.pageLayout.layoutParams = params
+                                        }
+                                    }
+
+                                }
+                            }
+
+                        }
+                )
 
                 return frameLayout
             } else {
