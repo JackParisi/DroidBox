06c8fc4e8673577017c404bf2b52a9e10a344130
Giacomo Parisi <giacomoparisi95@gmail.com>
2017-10-25 19:01:04 +0200

    Add settings params builder

--- droidbox/src/main/java/com/github/jackparisi/droidbox/wrapper/toolbar/ToolbarDroidSettings.kt
+++ droidbox/src/main/java/com/github/jackparisi/droidbox/wrapper/toolbar/ToolbarDroidSettings.kt
@@ -1,6 +1,9 @@
 package com.github.jackparisi.droidbox.wrapper.toolbar
 
 import android.content.Context
+import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import com.github.jackparisi.droidbox.architecture.model.DroidViewModel
 import com.github.jackparisi.droidbox.wrapper.DroidWrapperSettings
 import com.github.jackparisi.droidbox.wrapper.DroidWrapperView
@@ -15,4 +18,16 @@ class ToolbarDroidSettings(
         pageLayout: DroidWrapperView,
         wrapperLayout: DroidWrapperView,
         context: Context,
-        val overPageLayout: Boolean) : DroidWrapperSettings(viewModel, pageLayout, wrapperLayout, context)
+        val overPageLayout: Boolean) : DroidWrapperSettings(viewModel, pageLayout, wrapperLayout, context) {
+
+    companion object {
+
+        fun getMatchParentParams() = ViewGroup.LayoutParams(MATCH_PARENT, MATCH_PARENT)
+
+        fun getWrapContentParams() = ViewGroup.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
+
+        fun getMatchWrapParams() = ViewGroup.LayoutParams(MATCH_PARENT, WRAP_CONTENT)
+
+        fun getWrapMatchParams() = ViewGroup.LayoutParams(WRAP_CONTENT, MATCH_PARENT)
+    }
+}
