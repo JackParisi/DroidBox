0dee03237a15a3c0f4db7c53ba1ad5f88152c437
Giacomo Parisi <giacomoparisi95@gmail.com>
2017-12-14 17:33:02 +0100

    Convert dagger injection utilities to kotlin

---
+++ droidbox/src/main/java/com/github/giacomoparisi/droidbox/architecture/dagger/DroidDaggerInjector.kt
@@ -0,0 +1,62 @@
+package com.github.giacomoparisi.droidbox.architecture.dagger
+
+import android.app.Activity
+import android.app.Application
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentActivity
+import android.support.v4.app.FragmentManager
+
+import dagger.android.AndroidInjection
+import dagger.android.support.AndroidSupportInjection
+
+/**
+ * Created by Giacomo Parisi on 30/06/2017.
+ * https://github.com/giacomoParisi
+ */
+
+// Handle application dagger injection
+abstract class DroidDaggerInjector {
+
+    abstract fun injectComponent()
+
+    fun init(application: Application) {
+        injectComponent()
+        application
+                .registerActivityLifecycleCallbacks(object : Application.ActivityLifecycleCallbacks {
+                    override fun onActivityPaused(activity: Activity?) {}
+
+                    override fun onActivityResumed(activity: Activity?) {}
+
+                    override fun onActivityStarted(activity: Activity?) {}
+
+                    override fun onActivityDestroyed(activity: Activity?) {}
+
+                    override fun onActivitySaveInstanceState(activity: Activity?, outState: Bundle?) {}
+
+                    override fun onActivityStopped(activity: Activity?) {}
+
+                    override fun onActivityCreated(activity: Activity?, savedInstanceState: Bundle?) {
+                        activity?.let { handleInjection(it) }
+                    }
+
+                })
+    }
+
+    private fun handleInjection(activity: Activity) {
+        if (activity.javaClass.isAnnotationPresent(Injectable::class.java)) {
+            AndroidInjection.inject(activity)
+        }
+        if (activity is FragmentActivity) {
+            activity.supportFragmentManager.registerFragmentLifecycleCallbacks(
+                    object : FragmentManager.FragmentLifecycleCallbacks() {
+                        override fun onFragmentCreated(fm: FragmentManager?, f: Fragment?,
+                                                       savedInstanceState: Bundle?) {
+                            if (f!!.javaClass.isAnnotationPresent(Injectable::class.java)) {
+                                AndroidSupportInjection.inject(f)
+                            }
+                        }
+                    }, true)
+        }
+    }
+}
---
+++ droidbox/src/main/java/com/github/giacomoparisi/droidbox/architecture/dagger/Injectable.kt
@@ -0,0 +1,7 @@
+package com.github.giacomoparisi.droidbox.architecture.dagger
+
+/**
+ * Marks an activity / fragment injectable.
+ */
+@Retention(AnnotationRetention.RUNTIME)
+annotation class Injectable
