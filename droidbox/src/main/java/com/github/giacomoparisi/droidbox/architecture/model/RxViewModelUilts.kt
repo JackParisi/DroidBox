170aaa5c94fc815c26f91fd1147b33b911ba13f5
Giacomo Parisi <giacomoparisi95@gmail.com>
2018-05-10 14:30:48 +0200

    Added rx loading/error methods for Observables

--- droidbox/src/main/java/com/github/giacomoparisi/droidbox/architecture/model/RxViewModelUilts.kt
+++ droidbox/src/main/java/com/github/giacomoparisi/droidbox/architecture/model/RxViewModelUilts.kt
@@ -1,5 +1,6 @@
 package com.github.giacomoparisi.droidbox.architecture.model
 
+import io.reactivex.Observable
 import io.reactivex.Single
 
 /**
@@ -10,19 +11,29 @@ import io.reactivex.Single
 fun <T> loadingErrorRequest(
         viewModel: DroidViewModel,
         request: Single<T>,
-        error: (Throwable) -> Unit): Single<T> {
-    return request
-            .doOnSubscribe { viewModel.droidUIManager.showLoading() }
-            .doOnSuccess { viewModel.droidUIManager.hideLoading() }
-            .doOnError { error(it) }
-}
- fun <T> loadingRequest(viewModel: DroidViewModel, request: Single<T>): Single<T> {
-    return request
-            .doOnSubscribe { viewModel.droidUIManager.showLoading() }
-            .doFinally { viewModel.droidUIManager.hideLoading() }
-}
+        error: (Throwable) -> Unit): Single<T> =
+        request.doOnSubscribe { viewModel.droidUIManager.showLoading() }
+                .doOnSuccess { viewModel.droidUIManager.hideLoading() }
+                .doOnError { error(it) }
 
-fun <T> errorRequest(request: Single<T>, error: (Throwable) -> Unit): Single<T> {
-    return request
-            .doOnError { error(it) }
-}
+fun <T> loadingErrorRequest(
+        viewModel: DroidViewModel,
+        request: Observable<T>,
+        error: (Throwable) -> Unit): Observable<T> =
+        request.doOnSubscribe { viewModel.droidUIManager.showLoading() }
+                .doOnComplete { viewModel.droidUIManager.hideLoading() }
+                .doOnError { error(it) }
+
+fun <T> loadingRequest(viewModel: DroidViewModel, request: Single<T>): Single<T> =
+        request.doOnSubscribe { viewModel.droidUIManager.showLoading() }
+                .doFinally { viewModel.droidUIManager.hideLoading() }
+
+fun <T> loadingRequest(viewModel: DroidViewModel, request: Observable<T>): Observable<T> =
+        request.doOnSubscribe { viewModel.droidUIManager.showLoading() }
+                .doFinally { viewModel.droidUIManager.hideLoading() }
+
+fun <T> errorRequest(request: Single<T>, error: (Throwable) -> Unit): Single<T> =
+        request.doOnError { error(it) }
+
+fun <T> errorRequest(request: Observable<T>, error: (Throwable) -> Unit): Observable<T> =
+        request.doOnError { error(it) }
